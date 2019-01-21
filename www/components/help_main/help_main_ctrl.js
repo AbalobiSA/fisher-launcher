13f04f364dc51a64a69487873a83b0fa06a7b9f9
Courtney Jooste <courtney@codelab.io>
2019-01-21 12:53:42 +0200

    feat: added phone number to help submissions

--- www/components/help_main/help_main_ctrl.js
+++ www/components/help_main/help_main_ctrl.js
@@ -11,6 +11,7 @@ angular.module('app.controllers').controller('help_mainCtrl',
         'ISSUE_OTHER',
     ];
     $scope.desc = {};
+    $scope.phone = {};
     $scope.selected = {};
 
     $scope.openfn = __secrets.urls.openfn;
@@ -23,9 +24,13 @@ angular.module('app.controllers').controller('help_mainCtrl',
 
         if ($scope.selected.text === undefined || $scope.selected.text.trim().length === 0) {
             alert('Please select an issue!');
+        } else if ($scope.phone.text === undefined || $scope.phone.text.trim().length === 0) {
+            alert('Please add a phone number.');
+        } else if (!($scope.phone.text.trim().match(/^(\+\d{1,3}[- ]?)?\d{10}$/) && ! ($scope.phone.text.trim().match(/0{5,}/)))) {
+            alert('Please enter a valid phone number.');
         } else if ($scope.desc.text === undefined || $scope.desc.text.trim().length === 0) {
             alert('Please add a description.');
-        } else {
+        }  else {
             // console.log($scope.desc.text.trim());
             // console.log($scope.selected.text.trim());
 
@@ -35,6 +40,7 @@ angular.module('app.controllers').controller('help_mainCtrl',
                 data: {
                     'issue_filter': 'fisher_app',
                     'type': $scope.selected.text === undefined ? '' : $scope.selected.text.trim(),
+                    'phone': $scope.phone.text === undefined ? '' : $scope.phone.text.trim(),
                     'desc': $scope.desc.text === undefined ? '' : $scope.desc.text.trim()
                 }
             }).then(function (resp) {
